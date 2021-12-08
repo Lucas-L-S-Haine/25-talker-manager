eff09ed4a48ef8a952d16ec4ba97d4b2c1591cae
Lucas-L-S-Haine <lucaslshaine@gmail.com>
2021-12-08 11:21:31 -0300

    Requisito03 concluído.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs/promises');
-// const crypto = require('crypto-js');
+const crypto = require('crypto');
+
+  // const regEmail = /\w@\w.\w/;
+const regEmail = /\S+@\S+\.\S+/;
+const regPassword = /\w{6,}$/;
 
 const app = express();
 app.use(bodyParser.json());
 
-const HTTP_OK_STATUS = 200;
-const HTTP_NOT_OK_STATUS = 404;
 const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
-  response.status(HTTP_OK_STATUS).send();
+  response.status(200).send();
 });
 
 app.get('/talker', async (req, res) => {
   const talkerList = await fs.readFile('./talker.json', 'utf-8');
-  if (!talkerList) return res.status(HTTP_OK_STATUS).send([]);
-  return res.status(HTTP_OK_STATUS).send(JSON.parse(talkerList));
+  if (!talkerList) return res.status(200).send([]);
+  return res.status(200).send(JSON.parse(talkerList));
 });
 
 app.get('/talker/:id', async (req, res) => {
@@ -28,17 +30,30 @@ app.get('/talker/:id', async (req, res) => {
     .parse(talkerList)
     .find((talker) => (talker.id === Number(id)));
   if (!chosenTalker) {
-    return res.status(HTTP_NOT_OK_STATUS).send({
+    return res.status(404).send({
       message: 'Pessoa palestrante não encontrada',
     });
   }
-  return res.status(HTTP_OK_STATUS).send(chosenTalker);
+  return res.status(200).send(chosenTalker);
 });
 
-/* app.post('/login', (req, res, next) => {
-  const token = '7mqaVRXJSp886CGr';
-}); */
+app.post('/login', (req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  const { email, password } = req.body;
+  const validateEmail = regEmail.test(email);
+  const validatePassword = regPassword.test(password);
+  if (!email) return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+  if (!validateEmail) {
+    return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (!password) return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+  if (!validatePassword) {
+    return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  return res.status(200).send({ token });
+});
 
 app.listen(PORT, async () => {
   console.log('Online');
 });
+  // console.log(req.headers.token);
